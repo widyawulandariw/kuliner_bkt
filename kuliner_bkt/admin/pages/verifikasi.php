3a497f9d1d6ce42e25d8c0909888955abccb3bfe
Unknown <widyawulandari.wewe@gmail.com>
2018-09-07 20:54:08 +0700

    verifikasi ok

--- kuliner_bkt/admin/pages/verifikasi.php
+++ kuliner_bkt/admin/pages/verifikasi.php
@@ -4,5 +4,5 @@ include ('../../../connect.php');
 $edit = pg_query("update admin set role='C' where username='$_GET[user]'");
 
 if($edit){
-	header('location:http://localhost/kuliner_bkt/kuliner_bkt/admin/login.php');
+	header('location:http://localhost/kuliner_bkt/index.php');
 }
