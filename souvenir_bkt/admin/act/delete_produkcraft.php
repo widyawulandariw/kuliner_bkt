475f3c3c6e2c06818fca4a6039ec3fc2fcf5ecb6
Unknown <widyawulandari.wewe@gmail.com>
2018-09-13 21:17:07 +0700

    delete product souvenir & product craft

---
+++ souvenir_bkt/admin/act/delete_produkcraft.php
@@ -0,0 +1,20 @@
+<?php 
+include ('../../../connect.php');
+$id = $_GET['id'];
+
+$sql   = "DELETE from detail_product_small_industry where id_product='$id'";
+$sql2   = "DELETE from product_small_industry where id='$id'";
+
+$delete1 = pg_query($sql);
+if ($delete1) 
+{
+	$delete2 = pg_query($sql2);
+	echo "<script>alert ('Data Successfully Deleted');</script>";
+}
+else
+{
+	echo "<script>alert ('Error');</script>";
+}
+
+echo "<script>eval(\"parent.location='../?page=produkcraft'\");</script>";
+ ?>
